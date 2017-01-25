ef0aad099427158aa6c78f0bef79eb5f21e7bf6c
amax3002 <alexandremguy@gmil.com>
2017-01-25 17:16:47 -0500

    timeline still not working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def homepage
   end
+  
+  def timeline
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#homepage'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/timeline", controller: :welcome, action: :timeline, as: :timeline
+  root 'welcome#homepage'
 end
